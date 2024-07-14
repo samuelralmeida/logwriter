d55a036243dd47d083292d03d1b2b88216c000d2
samuelralmeida <samuel.ralmeida@gmail.com>
2024-07-13 22:10:08 -0300

    export logwriter type

--- logwriter.go
+++ logwriter.go
@@ -2,23 +2,23 @@ package logwriter
 
 import "os"
 
-type logWriter struct {
+type LogWriter struct {
 	file *os.File
 }
 
-func NewLogWriter(filename string) (*logWriter, error) {
+func NewLogWriter(filename string) (*LogWriter, error) {
 	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return nil, err
 	}
-	return &logWriter{file: file}, nil
+	return &LogWriter{file: file}, nil
 }
 
-func (l *logWriter) Close() error {
+func (l *LogWriter) Close() error {
 	return l.file.Close()
 }
 
-func (l *logWriter) Write(text string) error {
+func (l *LogWriter) Write(text string) error {
 	_, err := l.file.WriteString(text)
 	return err
 }
